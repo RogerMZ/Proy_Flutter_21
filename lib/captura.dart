3210e9753903bcf75d15e3472bb58137eca030b5
Rogelio Maldonado <rogermz.projects@gmail.com>
2021-12-22 20:27:32 -0600

    Agregando funcionalidad a WS

--- lib/captura.dart
+++ lib/captura.dart
@@ -4,6 +4,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class CapturaPage extends StatefulWidget {
   const CapturaPage({Key? key}) : super(key: key);
@@ -14,14 +16,18 @@ class CapturaPage extends StatefulWidget {
 
 class _CapturaPageState extends State<CapturaPage> {
   File? image;
+  String? base64img;
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
 
   _openGallery() async {
     var selectImage = await ImagePicker()
-        .pickImage(source: ImageSource.gallery, imageQuality: 10);
+        .pickImage(source: ImageSource.gallery, imageQuality: 1);
     setState(() {
       image = File(selectImage!.path);
       // log(image!.path);
       // log(base64Encode(image!.readAsBytesSync()));
+      base64img = base64Encode(image!.readAsBytesSync());
     });
     Navigator.of(context).pop();
   }
@@ -32,6 +38,7 @@ class _CapturaPageState extends State<CapturaPage> {
       image = File(selectImage!.path);
       // log(image!.path);
       // log(base64Encode(image!.readAsBytesSync()));
+      base64img = base64Encode(image!.readAsBytesSync());
     });
     Navigator.of(context).pop();
   }
@@ -80,67 +87,125 @@ class _CapturaPageState extends State<CapturaPage> {
     );
   }
 
+  Future<Position> _determinePosition() async {
+    bool serviceEnable;
+    LocationPermission permission;
+
+    serviceEnable = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnable) {
+      return Future.error('Locations services are disabled');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Locations services are disabled per user');
+      }
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  void _enviarRegistro() async {
+    Position position = await _determinePosition();
+    log(base64img!);
+    log(titleController.text);
+    log(descriptionController.text);
+    log("${position.latitude}");
+    log("${position.longitude}");
+
+    var url = Uri.parse(
+        'https://us-central1-cibi-40673.cloudfunctions.net/evidenciaapp');
+    var response = await http.post(url, body: {
+      'title': titleController.text,
+      'description': descriptionController.text,
+      'base64img': base64img,
+      'latitude': "${position.latitude}",
+      'longitude': "${position.longitude}",
+    });
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    // print(await http.read(Uri.parse('https://postman-echo.com/post')));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Uso de galería y camara'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            (image == null)
-                ? const Text('Sin imagen')
-                : Image.file(
-                    image!,
-                    width: 400,
-                    height: 400,
-                  ),
-            ElevatedButton(
-              onPressed: () {
-                _showSelectDialog();
-              },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Text('Seleccionar:'),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                  ),
-                  Icon(Icons.image),
-                  Padding(
-                    padding: EdgeInsets.all(8),
-                  ),
-                  Icon(Icons.camera_alt)
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Titulo',
-                  border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              (image == null)
+                  ? const Text('Sin imagen')
+                  : Image.file(
+                      image!,
+                      width: 400,
+                      height: 400,
+                    ),
+              ElevatedButton(
+                onPressed: () {
+                  _showSelectDialog();
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text('Seleccionar:'),
+                    Padding(
+                      padding: EdgeInsets.all(8),
+                    ),
+                    Icon(Icons.image),
+                    Padding(
+                      padding: EdgeInsets.all(8),
+                    ),
+                    Icon(Icons.camera_alt)
+                  ],
                 ),
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // _showSelectDialog();
-              },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Text('Enviar registro'),
-                  Padding(
-                    padding: EdgeInsets.all(8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextFormField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    labelText: 'Titulo',
+                    border: OutlineInputBorder(),
                   ),
-                  Icon(Icons.send_rounded)
-                ],
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextFormField(
+                  controller: descriptionController,
+                  decoration: const InputDecoration(
+                    labelText: 'Descripción',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _enviarRegistro();
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text('Enviar registro'),
+                    Padding(
+                      padding: EdgeInsets.all(8),
+                    ),
+                    Icon(Icons.send_rounded)
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
